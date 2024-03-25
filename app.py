8e312caaf57e3c828e4febc1ba3e5428c0b8d855
Sneha-Shajee-Mohan <snehasmohan8@gmail.com>
2024-03-25 12:22:35 -0400

    updating APP

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from utils.b2 import B2
 # ------------------------------------------------------
 #                      APP CONSTANTS
 # ------------------------------------------------------
-# REMOTE_DATA = 'coffee_analysis_w_sentiment.csv'
+REMOTE_DATA = 'NPS.ipynbnational_parks.csv'
 
 
 # ------------------------------------------------------
@@ -22,23 +22,16 @@ b2 = B2(endpoint=os.environ['B2_ENDPOINT'],
         key_id=os.environ['B2_KEYID'],
         secret_key=os.environ['B2_APPKEY'])
 
-st.write(os.environ['B2_ENDPOINT'])
-st.write(os.environ['B2_KEYID'])
-st.write(os.environ['B2_APPKEY'])
 # ------------------------------------------------------
 #                        CACHING
 # ------------------------------------------------------
-# @st.cache_data
-# def get_data():
+@st.cache_data
+def get_data():
 #     # collect data frame of reviews and their sentiment
-#     b2.set_bucket(os.environ['B2_BUCKETNAME'])
-#     df_coffee = b2.get_df(REMOTE_DATA)
-
-#     # average sentiment scores for the whole dataset
-#     benchmarks = df_coffee[['neg', 'neu', 'pos', 'compound']] \
-#                     .agg(['mean', 'median'])
+    b2.set_bucket(os.environ['B2_BUCKETNAME'])
+    df = b2.get_df(REMOTE_DATA)
     
-#     return df_coffee, benchmarks
+    return df
 
 
 
@@ -50,60 +43,10 @@ st.write(os.environ['B2_APPKEY'])
 # ------------------------------
 st.write(
 '''
-# Review Sentiment Analysis
+# national park portal
 We pull data from our Backblaze storage bucket, and render it in Streamlit.
 ''')
 
-df_coffee = get_data()
+df_park = get_data()
+st.write(df_park.sample(5))
 
-
-# ------------------------------
-# PART 1 : Filter Data
-# ------------------------------
-# roast = st.selectbox("Select a roast:",
-#                      df_coffee['roast'].unique())
-
-# loc_country = st.selectbox("Select a roaster location:",
-#                      df_coffee['loc_country'].unique())
-
-# df_filtered = filter_coffee(roast, loc_country, df_coffee)
-
-# st.write(
-# '''
-# **Your filtered data:**
-# ''')
-
-# st.dataframe(df_filtered)
-
-# ------------------------------
-# PART 2 : Plot
-# ------------------------------
-
-# st.write(
-# '''
-# ## Visualize
-# Compare this subset of reviews with the rest of the data.
-# '''
-# )
-
-# fig = plot_sentiment(df_filtered, benchmarks)
-# st.plotly_chart(fig)
-
-# ------------------------------
-# PART 3 : Analyze Input Sentiment
-# ------------------------------
-
-# st.write(
-# '''
-# ## Custom Sentiment Check
-
-# Compare these results with the sentiment scores of your own input.
-# '''
-# )
-
-# text = st.text_input("Write a paragraph, if you like.", 
-#                      "Your text here.")
-
-# df_sentiment = get_sentence_sentiment(text, analyzer)
-
-# st.dataframe(df_sentiment)
